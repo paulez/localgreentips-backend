9acec874fb0120a69e68e807196ec11ec634ac02
Paul Ezvan <paul@ezvan.fr>
2019-11-17 20:55:28 -0800

    Admin: add search on tip location.

--- localgreentips/tips/admin.py
+++ localgreentips/tips/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Tip, Comment
 
-admin.site.register(Tip)
+
+class TipAdmin(admin.ModelAdmin):
+    search_fields = ('cities__name', 'subregions__name',
+                     'regions__name', 'countries__name')
+
+
+admin.site.register(Tip, TipAdmin)
 admin.site.register(Comment)
